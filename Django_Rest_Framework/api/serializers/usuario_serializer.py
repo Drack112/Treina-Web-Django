a584bbc322c22f140758c9228f829d2595ba9c77
Drack112 <onepessoa3@gmail.com>
2022-02-26 20:58:59 -0300

    Django Rest

---
+++ Django_Rest_Framework/api/serializers/usuario_serializer.py
@@ -0,0 +1,15 @@
+from rest_framework import serializers
+from django.contrib.auth.models import User
+
+
+class UsuarioSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ("username", "password")
+
+    # Criptografar Senha
+    def create(self, validated_data):
+        user = super(UsuarioSerializer, self).create(validated_data)
+        user.set_password(validated_data["password"])
+        user.save()
+        return user
